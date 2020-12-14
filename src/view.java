fecae37a5fc27661c872f0a4864b84bc25039ae6
Gabriel <gabrieloberg3@gmail.com>
2020-12-14 11:14:38 +0100

    jag har gjort entry jobbat på viewn

--- src/view.java
+++ src/view.java
@@ -2,4 +2,8 @@ import javax.swing.*;
 
 public class view {
     private JPanel panel;
+    private JButton newEntryButton;
+    private JButton loadButton;
+    private JButton saveButton;
+    private JTextArea textArea1;
 }
