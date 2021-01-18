5605545f634cd3adf79cad86f4cd7db9e97a059c
Gabriel <gabrieloberg3@gmail.com>
2021-01-18 11:14:52 +0100

    jag la till en javadocs kommentar

--- src/author.java
+++ src/author.java
@@ -1,6 +1,11 @@
+/**
+ * This class stores information about a person
+ */
+
 public class author {
     private String firstName;
     private String lastName;
+
 
     public author(String firstName, String lastName) {
         this.firstName = firstName;
